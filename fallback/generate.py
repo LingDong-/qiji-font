8157d83a31159a8216c74df9da05622a423230d9
LingDong- <lingdong0618@hotmail.com>
2020-02-05 22:22:00 -0500

    compress png

--- fallback/generate.py
+++ fallback/generate.py
@@ -13,27 +13,29 @@ def make_char(ch):
 
 	im = np.array(im)
 	im00 = im.copy()
-	cv.imshow('',im);cv2.waitKey(0)
+	# cv.imshow('',im);cv2.waitKey(0)
 
 	im = im.astype(np.float32)/255;
 
+	im = cv2.resize(im,(522,640))[64:-64,5:-5]
+
 	for i in range(512):
-		r = i*0.1*(0.95+0.05*math.sin(i*0.1))*0.7
+		r = i*0.1*(0.95+0.05*math.sin(i*0.1))*0.5
 		# r = random.random()*20
 		im[52:460,i]=im[int(52+r):int(460+r),i]
-	cv.imshow('',im);cv2.waitKey(0)
+	# cv.imshow('',im);cv2.waitKey(0)
 
 	im0 = im.copy()
 
 	im -= np.random.random((512,512))*0.1
 	m2 = cv2.GaussianBlur(np.random.random((64,64)),(11,11),0)
-	cv.imshow('',m2);cv2.waitKey(0)
+	# cv.imshow('',m2);cv2.waitKey(0)
 	im +=  cv2.GaussianBlur(cv2.resize(m2*1.1,(512,512),interpolation=cv2.INTER_AREA),(11,11),0)
 
 
 	im = np.clip(im,0,1)
 	im0 = cv2.dilate(im0,np.array([[0,1,0],[1,1,1],[0,1,0]],np.uint8),iterations=4)
-	cv.imshow('',im);cv2.waitKey(0)
+	# cv.imshow('',im);cv2.waitKey(0)
 
 	_,im = cv2.threshold(im,0.5,1,cv2.THRESH_BINARY)
 	im = cv2.erode(im,np.array([[0,1,0],[1,1,1],[0,1,0]],np.uint8))
@@ -42,15 +44,22 @@ def make_char(ch):
 
 	im = cv2.GaussianBlur(im,(21,21),0)
 	_,im = cv2.threshold(im,0.5,1,cv2.THRESH_BINARY)
-	im = cv2.dilate(im,np.array([[0,1,0],[0,1,0],[0,1,0]],np.uint8),iterations=1)
-	im = cv2.erode(im,np.array([[0,0,0],[1,1,1],[0,0,0]],np.uint8),iterations=1)
+	# im = cv2.dilate(im,np.array([[0,1,0],[0,1,0],[0,1,0]],np.uint8),iterations=1)
+	im = cv2.erode(im,np.array([[0,0,0],[1,1,1],[0,0,0]],np.uint8),iterations=2)
 
-	m3 = cv2.GaussianBlur(cv2.resize(cv2.GaussianBlur(np.random.random((64,64)),(11,11),0),(512,512),interpolation=cv2.INTER_AREA),(51,51),0)
+	m3 = cv2.GaussianBlur(cv2.resize(cv2.GaussianBlur(np.random.random((16,16)),(11,11),0),(512,512),interpolation=cv2.INTER_AREA),(51,51),0)
 	m4 = cv2.GaussianBlur(cv2.resize(np.random.random((256,256)),(512,512),interpolation=cv2.INTER_AREA),(11,11),0)
 	_,m5 = cv2.threshold(m3*m4,0.18,1,cv2.THRESH_BINARY)
+
+	m5 = cv2.dilate(m5,np.array([[0,1,0],[1,1,1],[0,1,0]],np.uint8),iterations=2)
+	m5 = cv2.erode(m5,np.array([[0,1,0],[1,1,1],[0,1,0]],np.uint8),iterations=2)
+	# m5 = cv2.erode(m5,np.array([[1,0,0],[1,1,0],[0,1,0]],np.uint8),iterations=1)
+	# m5 = cv2.erode(m5,np.array([[0,1,1,1,0],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[0,1,1,1,0]],np.uint8),iterations=1)
+	# cv.imshow('',m5);cv2.waitKey(0)
+
 	im = cv.morphologyEx(im, cv.MORPH_CLOSE, np.array([[0,1,0],[1,1,1],[0,1,0]],np.uint8))
 
-	cv.imshow('',m5);cv2.waitKey(0)
+	
 	
 
 	# im+=m3
@@ -59,15 +68,27 @@ def make_char(ch):
 	im = (np.clip(1-im,0,1)*255).astype(np.uint8)
 
 
-	cv.imshow('',255-im);cv2.waitKey(0)
+	# cv.imshow('',255-im);cv2.waitKey(0)
 
 	cv.imshow('',np.hstack((255-im00,im)));cv2.waitKey(0)
 	return im
-make_char("姜")
+
+CH0 = 0x4e00
+CH1 = 0x9feb
+
+lbls = [x.split("\t") for x in open("../data/labels_all.txt",'r').read().split("\n")]
+
+for p,l in lbls:
+	im0 = make_char(l)
+	im1 = cv2.imread("../output/fine/"+p.replace(".png",".bmp"),0)
+
+	# cv2.imshow('',np.hstack((im0,im1)));cv2.waitKey(0)
+
+for i in range(20):
+	c = chr(random.randrange(CH0,CH1))
+	make_char(c)
 
 
-# CH0 = 0x4e00
-# CH1 = 0x9feb
 
 # for i in range(CH0,CH1):
 # 	c = chr(i)
